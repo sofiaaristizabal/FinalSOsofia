764667a8c5eb4df5cb96cc3a00dec26e36451e31
sofiaaristizabal <sofiaaristizabal23@gmail.com>
2025-11-11 08:17:54 -0500

    changes

--- punto1/script.sh
+++ punto1/script.sh
@@ -1,8 +1,8 @@
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 431151642822.dkr.ecr.us-east-1.amazonaws.com
 
-docker build --platform linux/amd64 --provenance=false -t sam_final_so .
+docker build --platform linux/amd64 --provenance=false -t sofia_final_so .
 
-docker tag sam_final_so:latest 431151642822.dkr.ecr.us-east-1.amazonaws.com/sam_final_so:latest
+docker tag sam_final_so:latest 431151642822.dkr.ecr.us-east-1.amazonaws.com/sofia_final_so:latest
 
-docker push 431151642822.dkr.ecr.us-east-1.amazonaws.com/sam_final_so:latest
+docker push 431151642822.dkr.ecr.us-east-1.amazonaws.com/sofia_final_so:latest
